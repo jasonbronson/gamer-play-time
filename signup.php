a2f82e7a3b889681c9b5b113520af521bd5a48f6
jbronson@gxm.com <jasonbronson@gmail.com>
2019-02-12 16:09:00 -0500

    update email

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $comments = $_POST['comments'];
 
-mail("jasonbronson@gmail.com", "gamerplaytime signup", "$name, $email, $comments");
+mail("jasonbronson+gamerplaytime@gmail.com", "gamerplaytime signup", "$name, $email, $comments");
 
 error_log("Recording $name");
 echo json_encode("Check your email for further details");
